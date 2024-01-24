44b3a99cf8a818871883e7e10fc1d09c43fe8852
Andrew Olson <ols21007@byui.edu>
2024-01-24 14:22:53 -0700

    Added learning activities and menu and google map components.

--- scripts/menubutton.js
+++ scripts/menubutton.js
@@ -1,5 +1,6 @@
-const menuButton = document.querySelector('menu');
+const menuButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
 
-menuButton.addEventListener('click', () => 
-{navigation.classList.toggle('open'); menuButton.classList.toggle('open)');});
+menuButton.addEventListener('click', () => {
+    navigation.classList.toggle('open'); 
+    menuButton.classList.toggle('open');});
