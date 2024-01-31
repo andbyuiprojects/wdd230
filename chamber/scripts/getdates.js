c96209ed05c5a480762faf11f6d11fe47196500e
Andrew Olson <ols21007@byui.edu>
2024-01-31 15:16:16 -0700

    Removed unnecessary code

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -1,7 +1,3 @@
-// Copy write year
-var year = new Date();
-document.querySelector("#currentYear").textContent = year.getFullYear();
-
 // When the document was last changed
 let lastChanged  = new Date(document.lastModified);
 document.querySelector("#lastModified").textContent = lastChanged ;
