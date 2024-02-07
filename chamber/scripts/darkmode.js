920c83a8a9b4fdf5f7f66672144c92bb6258fe71
Andrew Olson <ols21007@byui.edu>
2024-02-07 15:05:59 -0700

    Updated so all elements would be darkmode

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -1,27 +1,67 @@
 const modeButton = document.querySelector("#mode");
-const main = document.querySelector("main");
-const body = document.querySelector("body");
-// const header = document.querySelector("header");
-// const nav = document.querySelector("nav");
-// const footer = document.querySelector("footer");
-// const card = document.querySelector("card");
 
+const body = document.querySelector("body");
+const header = document.querySelector("header");
+const main = document.querySelector("main");
+const h1 = document.querySelector("h1");
+const nav = document.querySelector("nav");
+
+const home = document.querySelector("#home");
+const discover = document.querySelector("#discover");
+const directory = document.querySelector("#directory");
+const join = document.querySelector("#joinTab");
+
+const weather = document.querySelector("#weath");
+const currentevents = document.querySelector("#curr");
+const spotlight = document.querySelector("#spot");
+
+const footer = document.querySelector("footer");
 
 modeButton.addEventListener("click", () => {
 	if (modeButton.textContent.includes("🌃")) {
-		body.style.background = "#161118";
-		// header.style.background = "#25351C";
-		// nav.style.background = "#192B39";
-		// footer.style.background = "#523A00";
+		body.style.background = "#2D222F";
+		header.style.background = "#4A6B38";
+		nav.style.background = "#325971";
+		footer.style.background = "#664900";
+		
+		weather.style.background = "#325971";
+		currentevents.style.background = "#325971";
+		spotlight.style.background = "#325971";
+
+		h1.style.color = "#fff";
 		main.style.color = "#fff";
-        // nav.style.color = "#fff";
+        footer.style.color = "#fff";
+
+		home.style.color = "#fff";
+		discover.style.color = "#fff";
+		directory.style.color = "#fff";
+		join.style.color = "#fff";
+
+		weather.style.color = "#fff";
+		currentevents.style.color = "#fff";
+		spotlight.style.color = "#fff";
+
 		modeButton.textContent = "🌆";
 	} else {
 		body.style.background = "#D6CBD9";
-		// header.style.background = "#85B36B";
-        // nav.style.background = "#8EB5CD";
-		// footer.style.background = "#ffb703";
+		header.style.background = "#85B36B";
+        nav.style.background = "#8EB5CD";
+		footer.style.background = "#ffb703";
+
+		h1.style.color = "#000";
 		main.style.color = "#000";
+		footer.style.color = "#000";
+
+		discover.style.color = "#000";
+		directory.style.color = "#000";
+		join.style.color = "#000";
+
+		weather.style.color = "#000";
+		currentevents.style.color = "#000";
+		spotlight.style.color = "#000";
+
+
+
 		modeButton.textContent = "🌃";
 	}
 });
