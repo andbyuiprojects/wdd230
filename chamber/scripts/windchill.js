a16d9868f048d4ec8f4568400a19451ec20ba832
Andrew Olson <ols21007@byui.edu>
2024-02-17 16:08:19 -0700

    Worked on the wind-chill factor

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -4,3 +4,15 @@ var windChill= (35.74 + (0.6215 * temp)) - (35.75 * Math.pow(windSpeed, 0.16)) +
 
 var windChill= Math.round(windChill);
 document.getElementById("windchill").innerHTML= windChill;
+
+// function windChill (temp, windChill) {
+//     let windChill= 0;
+//     if (temp <= 50 && windSpeed > 3) {
+//         let windChill = windChillCalculate();
+//     }
+    
+// }
+
+// function windChillCalculate() {
+//     return (35.74 + (0.6215 * temp)) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temp * Math.pow(windSpeed, 0.16));
+// }
