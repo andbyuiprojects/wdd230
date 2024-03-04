f876d6289c6fbb1e53794d1698c024da95030fb9
Andrew Olson <ols21007@byui.edu>
2024-03-04 16:12:56 -0700

    Completed fetch API assignment

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -7,7 +7,37 @@ async function getProphetData() {
     // Converts the response to a JSON object using the .json method and stores it in data
     const data = await response.json();
     // Checks the data response at this point in the console window
-    console.table(data.prophets);
+    //console.table(data.prophets);
+    displayProphets(data.prophets);
 }
-  
+
+const displayProphets = (prophets) => {
+    // Process each prophet record one at a time, creating a new card each time
+    prophets.forEach((prophet) => {
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let portrait = document.createElement('img');
+        let birthDate = document.createElement('h4');
+        let birthPlace = document.createElement('h4');
+
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.fullName}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        birthDate.textContent = `Date of Birth: ${prophet.birthdate}`
+        birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`
+        
+        card.appendChild(fullName);
+        card.appendChild(birthDate);
+        card.appendChild(birthPlace);
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    } );
+}
+
 getProphetData();
