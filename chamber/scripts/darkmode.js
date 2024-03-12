7aacfaa00d2608fa2ad2c46a0c767b22ea023a73
Andrew Olson <ols21007@byui.edu>
2024-03-12 15:51:27 -0600

    Code optimization

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -1,8 +1,8 @@
 const modeButton = document.querySelector("#mode");
 
-const body = document.querySelector("body");
+
 const header = document.querySelector("header");
-const main = document.querySelector("main");
+
 const h1 = document.querySelector("h1");
 const nav = document.querySelector("nav");
 
@@ -11,25 +11,15 @@ const discover = document.querySelector("#discover");
 const directory = document.querySelector("#directory");
 const join = document.querySelector("#joinTab");
 
-const weather = document.querySelector("#weath");
-const currentevents = document.querySelector("#curr");
-const spotlight = document.querySelector("#spot");
-
 const footer = document.querySelector("footer");
 
 modeButton.addEventListener("click", () => {
 	if (modeButton.textContent.includes("🌃")) {
-		body.style.background = "#2D222F";
 		header.style.background = "#4A6B38";
 		nav.style.background = "#325971";
 		footer.style.background = "#664900";
-		
-		weather.style.background = "#325971";
-		currentevents.style.background = "#325971";
-		spotlight.style.background = "#325971";
-
+	
 		h1.style.color = "#fff";
-		main.style.color = "#fff";
         footer.style.color = "#fff";
 
 		home.style.color = "#fff";
@@ -37,34 +27,19 @@ modeButton.addEventListener("click", () => {
 		directory.style.color = "#fff";
 		join.style.color = "#fff";
 
-		weather.style.color = "#fff";
-		currentevents.style.color = "#fff";
-		spotlight.style.color = "#fff";
-
 		modeButton.textContent = "🌆";
 	} else {
-		body.style.background = "#D6CBD9";
 		header.style.background = "#85B36B";
         nav.style.background = "#8EB5CD";
 		footer.style.background = "#ffb703";
-		weather.style.background = "#8EB5CD";
-		currentevents.style.background = "#8EB5CD";
-		spotlight.style.background = "#8EB5CD";
 
 		h1.style.color = "#000";
-		main.style.color = "#000";
 		footer.style.color = "#000";
 
 		home.style.color = "#000";
 		discover.style.color = "#000";
 		directory.style.color = "#000";
 		join.style.color = "#000";
-
-		weather.style.color = "#000";
-		currentevents.style.color = "#000";
-		spotlight.style.color = "#000";
-
-
 
 		modeButton.textContent = "🌃";
 	}
--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -3,7 +3,7 @@ let windSpeed= 11;
 var windChill= (35.74 + (0.6215 * temp)) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temp * Math.pow(windSpeed, 0.16));
 
 var windChill= Math.round(windChill);
-document.getElementById("windchill").innerHTML= windChill;
+// document.getElementById("windchill").innerHTML= windChill;
 
 // function windChill (temp, windChill) {
 //     let windChill= 0;
