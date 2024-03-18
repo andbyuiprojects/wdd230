83f47eb61b121aab765c87d520c6027b39c9ac68
Andrew Olson <ols21007@byui.edu>
2024-03-18 16:49:06 -0600

    Worked on Banner displaying properly

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -1,16 +1,16 @@
 function bannerDay() {
-    const today = new Date().getDay();
-    return [1, 2, 5].includes(today);
+  const today = new Date().getDay();
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const currentDay = bannerDay();
+  if (currentDay == 1 || currentDay == 2 || currentDay == 3) 
+  {
+    document.getElementById("announcement").display = "flex";
   }
-  
-  function closeBanner() {
-    const banner = document.getElementById("announcement");
-    banner.style.display = "none";
-  }
-  
-  document.addEventListener("DOMContentLoaded", function () {
-    if (bannerDay()) {
-      const banner = document.getElementById("announcement");
-      banner.style.display = "flex";
-    }
-  });
+});
+
+function closeBanner() {
+  const banner = document.getElementById("announcement");
+  banner.style.display = "none";
+}
