78063ce4ec96bd47ccce1656d415e5c0693707dc
Andrew Olson <ols21007@byui.edu>
2024-03-28 12:59:46 -0600

    Created table showing pricing info

--- scoots/scripts/pricing.js
+++ scoots/scripts/pricing.js
@@ -1,13 +1,50 @@
-const baseURL = "https://andbyuiprojects.github.io/wdd230/scoots/index";
+const url = "data/pricing.json";
+const table = document.querySelector("#table");
 
-const pricingURL = "https://andbyuiprojects.github.io/wdd230/scoots/data/pricing.json";
-
-const cards = document.querySelector("#cards");
-
-async function getPrices() {
-    const response = await fetch(linksURL);
-
+async function getVehicleData() {
+    // Stores the response from the fetch Method in response
+    const response = await fetch(url);
+    // Converts the response to a JSON object using the .json method and stores it in data
     const data = await response.json();
-
-    displayPrices(data);
+    console.table(data.vehicles);
+    displayVehicles(data.vehicles);
 }
+
+const displayVehicles = (vehicles) => {
+
+    let title = document.createElement("h2");
+    title.textContent = "Rental Pricing";
+    table.appendChild(title);
+    
+    vehicles.forEach((vehicle) => {
+        let rowHead = document.createElement("tr");
+        let name = document.createElement("td");
+        let halfRez = document.createElement("td");
+        let fullRez = document.createElement("td");
+        let halfWalk = document.createElement("td");
+        let fullWalk = document.createElement("td");
+
+        name.textContent = `${vehicle.type}`;
+        halfRez.textContent = `${vehicle.halfrez}`;
+        fullRez.textContent = `${vehicle.fullrez}`;
+        halfWalk.textContent = `${vehicle.halfwalk}`;
+        fullWalk.textContent = `${vehicle.fullwalk}`;
+ 
+        rowHead.appendChild(name);
+        rowHead.appendChild(halfRez);
+        rowHead.appendChild(fullRez);
+        rowHead.appendChild(halfWalk);
+        rowHead.appendChild(fullWalk);
+
+        if (table.appendChild(rowHead) != null) {
+            table.appendChild(rowHead);
+        }
+        else {
+            console.log("Error, value is null!");
+        }
+
+    });   
+
+};
+
+getVehicleData();
